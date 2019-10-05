43184e890081596c6ae7a1a6e6ea486527e9f9af
Keillan Cruickshank <cruickshankxkeillan90@gmail.com>
2019-10-05 17:14:21 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -4,5 +4,5 @@ second_number = 5
 
 sum = first_number + second_number
 difference = first_number-second_number
-multiplication = first_number * second_number
+product = first_number * second_number
 division = first_number / second_number
